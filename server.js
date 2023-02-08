3c0b837acd203c73c3747885e39cbdf77745da7c
Gustavo Costa Neves <gustavo.neves@thoughtworks.com>
2023-02-08 13:48:56 -0300

    fix: fixing vercel

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('/tmp/db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(jsonServer.bodyParser)
-
 server.post('/todo', (req, res) => {
     const db = router.db.getState().todo
     const newTodo = {
